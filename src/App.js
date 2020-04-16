3c001477859e9cebe35d3da313878f55a77c34ed
MatthewSchuette <mjschuette4@gmail.com>
2020-04-16 18:02:53 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -16,18 +18,19 @@ function App() {
 	};
 
 	return (
+		<ProductContext.Provider value ={{ products, addItem }}>
+		<CartContext.Provider value={cart}>
 		<div className="App">
-			<Navigation cart={cart} />
+			<Navigation />
 
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+			
+			<Route exact path="/" component={Products} />
+			<Route path="/cart" component={ShoppingCart} />
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
 		</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,1 +1,5 @@
 import {createContext} from 'react';
+
+const ProductContext = createContext();
+
+export default ProductContext; 
